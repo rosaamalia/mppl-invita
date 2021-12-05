07bb877ca6ad4e91cdb607589a4b552421c8dad0
rosaamalia <amaliarosaaa15@gmail.com>
2021-12-05 14:33:59 +0700

    menambahkan framework

---
+++ mppl-invita/resources/views/components/navbar.blade.php
@@ -0,0 +1,73 @@
+<nav class="navbar navbar-expand-lg navbar-dark shadow-sm sticky-top" style="background-color: #0199ff; z-index: 99999">
+    <div class="container d-flex">
+      <a class="navbar-brand ms-0" href="/" style="font-size: 3.2rem"><strong>Invita.</strong>id</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav align-items-end mx-auto list" style="font-size: 2rem">
+          <li class="nav-item pe-lg-5 pe-sm-2 pe-3 home">
+            <div class="row">
+              <a class="nav-link text-white" aria-current="page" href="/">Home</a>
+            </div>
+            <div class="row">
+              <div class="line" style="background-color: white; width: 100%; height: 5px; border-radius: 8px"></div>
+            </div>
+          </li>
+          <li class="nav-item pe-lg-5 pe-sm-2 pe-3 order">
+            <div class="row">
+              <a class="nav-link text-white order" href="#order">Order</a>
+            </div>
+            <!-- <div class="row">
+                <div class="line" style="background-color: white; width: 100%; height: 5px; border-radius: 8px"></div>
+              </div> -->
+          </li>
+          <li class="nav-item pe-lg-5 pe-sm-2 pe-3 portfolio">
+            <div class="row">
+              <a class="nav-link text-white portfolio" href="/portfolio">Portfolio</a>
+            </div>
+            <!-- <div class="row">
+                <div class="line" style="background-color: white; width: 100%; height: 5px; border-radius: 8px"></div>
+              </div> -->
+          </li>
+          <li class="nav-item pe-lg-5 pe-sm-2 pe-3 blog">
+            <div class="row">
+              <a class="nav-link text-white blog" href="#blog">Blog</a>
+            </div>
+            <!-- <div class="row">
+              <div class="line" style="background-color: white; width: 100%; height: 5px; border-radius: 8px"></div>
+          </div> -->
+          </li>
+          <li class="nav-item pe-lg-5 pe-sm-2 pe-3 review">
+            <div class="row">
+              <a class="nav-link text-white review" href="#review">Review</a>
+            </div>
+            <!-- <div class="row">
+                  <div class="line" style="background-color: white; width: 100%; height: 5px; border-radius: 8px"></div>
+              </div> -->
+          </li>
+          <li class="nav-item pe-lg-5 pe-sm-2 pe-3 faq">
+            <div class="row">
+              <a class="nav-link text-white faq" href="#faq">FAQ</a>
+            </div>
+            <!-- <div class="row">
+                <div class="line" style="background-color: white; width: 100%; height: 5px; border-radius: 8px"></div>
+              </div> -->
+          </li>
+          <!-- <div class="container animation start-home position-absolute"></div> -->
+        </ul>
+        <ul class="navbar-nav align-items-end text-white">
+          <li class="nav-item pe-lg-2 pe-sm-0">
+            <button class="btn fw-bold" style="font-size: 1.5rem">
+              <a class="nav-link active" href="/masuk"> Masuk</a>
+            </button>
+          </li>
+          <li class="nav-item pe-lg-2 pe-sm-0">
+            <button class="btn btn-light fw-bold" style="font-size: 1.5rem">
+              <a class="nav-link active" href="/daftar" style="color: #0081d8">Daftar</a>
+            </button>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
---
+++ mppl-invita/resources/views/daftar.blade.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Daftar Invita.id</title>
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.6.0/font/bootstrap-icons.css" />
+
+    <!-- css -->
+    <link rel="stylesheet" href="css/daftar.css" />
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+  </head>
+  <body>
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    -->
+
+    <!-- navbar -->
+    @include('components/navbar')
+    <!-- end navbar -->
+
+    <!-- Login -->
+    <div class="container-fluid" style="padding-top: 150px; padding-bottom: 150px">
+      <div class="container">
+        <div class="row">
+          <div class="col-6 d-md-block d-none text-center">
+            <img class="img-fluid" src="img/phone-daftar-masuk.png" alt="" class="img-fluid" />
+          </div>
+          <div class="col-md-6 col-sm-12 text-center d-flex flex-column">
+            <div class="row">
+              <h1 class="text-white" style="font-size: 5rem"><strong>Invita.</strong>id</h1>
+            </div>
+            <div class="row justify-content-center mt-5 pt-4">
+              <div class="col-xxl-7 col-xl-8 col-lg-10 col-sm-11 col-11 bg-white pt-2 pb-2 position-relative" style="border-radius: 10px">
+                <button type="button" class="btn text-start text-black fs-3 w-100" style="padding-left: 60px; color: #ccc">Daftar dengan google</button>
+                <span class="icon-form bi bi-google fs-1 ps-3 pe-3 pt-1 position-absolute" style="top: 0; left: 0; height: 100%"></span>
+              </div>
+            </div>
+            <div class="row mt-3">
+              <div class="col-12 fs-2" style="color: rgb(255, 255, 255)"><p>atau</p></div>
+            </div>
+            <div class="row justify-content-center mt-3">
+              <div class="col-xxl-7 col-xl-8 col-lg-10 col-sm-11 col-11 bg-white pt-2 pb-2 position-relative" style="border-radius: 10px">
+                <input type="text" class="form-control text-start text-black fs-3 w-100 border-0" style="padding-left: 60px; color: #ccc; border-radius: 10px" placeholder="Nama Lengkap" />
+                <span class="icon-form material-icons fs-1 ps-3 pe-3 pt-3 position-absolute" style="top: 0; left: 0; height: 100%"> badge </span>
+              </div>
+            </div>
+            <div class="row justify-content-center mt-3">
+              <div class="col-xxl-7 col-xl-8 col-lg-10 col-sm-11 col-11 bg-white pt-2 pb-2 position-relative" style="border-radius: 10px">
+                <input type="email" class="form-control text-start text-black fs-3 w-100 border-0" style="padding-left: 60px; color: #ccc; border-radius: 10px" placeholder="Email" />
+                <i class="icon-form bi bi-envelope-fill fs-1 ps-3 pe-3 pt-1 position-absolute" style="top: 0; left: 0; height: 100%"></i>
+              </div>
+            </div>
+            <div class="row justify-content-center mt-3">
+              <div class="col-xxl-7 col-xl-8 col-lg-10 col-sm-11 col-11 bg-white pt-2 pb-2 position-relative" style="border-radius: 10px">
+                <input type="tel" class="form-control text-start text-black fs-3 w-100 border-0" style="padding-left: 60px; color: #ccc; border-radius: 10px" placeholder="Nomor Handphone" />
+                <i class="icon-form bi bi-telephone-fill fs-1 ps-3 pe-3 pt-1 position-absolute" style="top: 0; left: 0; height: 100%"></i>
+              </div>
+            </div>
+            <div class="row justify-content-center mt-4">
+              <div class="col-xxl-7 col-xl-8 col-lg-10 col-sm-11 col-11 bg-white pt-2 pb-2 position-relative" style="border-radius: 10px">
+                <input type="password" id="pass" name="password" class="form-control text-start text-black fs-3 w-100 border-0" placeholder="Kata Sandi" style="padding-left: 60px; color: #ccc; border-radius: 10px" />
+                <i class="icon-form bi bi-key-fill fs-1 ps-3 pe-3 pt-1 position-absolute" style="top: 0; left: 0; height: 100%"></i>
+              </div>
+            </div>
+            <div class="row justify-content-center mt-4">
+              <div class="col-xxl-7 col-xl-8 col-lg-10 col-sm-11 col-11 bg-white pt-2 pb-2 position-relative" style="border-radius: 10px">
+                <input
+                  type="password"
+                  id="confirm-pass"
+                  name="confirm-password"
+                  class="form-control text-start text-black fs-3 w-100 border-0"
+                  placeholder="Konfirmasi Kata Sandi"
+                  style="padding-left: 60px; color: #ccc; border-radius: 10px"
+                />
+                <i class="icon-form bi bi-key-fill fs-1 ps-3 pe-3 pt-1 position-absolute" style="top: 0; left: 0; height: 100%"></i>
+              </div>
+            </div>
+            <div class="row justify-content-center mt-5 pt-3">
+              <div class="col-12" style="border-radius: 10px">
+                <button type="button" class="btn text-center text-white fw-normal ps-4 pe-4 fs-3 pt-2 pb-2" style="background-color: #044581">Daftar</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- End Login -->
+  </body>
+</html>
